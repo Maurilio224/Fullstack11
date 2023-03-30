899eb1e42db25609c9cbe84a7787d91ed7d63783
Maurilio Farina <farinamaurilio93@gmail.com>
2023-03-30 18:05:48 +0200

    es 18 complete

---
+++ esercizio-18/exercise-3.js
@@ -0,0 +1,19 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  static fromObject(array) {
+    let fullName = new Person(array.firstName, array.lastName)
+    return fullName
+  }
+
+}
+
+const obj = {
+  firstName: "Mario",
+  lastName: "Rossi",
+};
+
+const person = Person.fromObject(obj);
+console.log(person.firstName + " " + person.lastName);
