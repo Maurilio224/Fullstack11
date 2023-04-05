364cd430087b40395993b251f5510af7a69f4aff
Maurilio Farina <farinamaurilio93@gmail.com>
2023-04-05 15:54:06 +0200

    ex 26:complete

---
+++ esercizio-26/exercise-1.js
@@ -0,0 +1,10 @@
+//function sum(num1, num2, num3, num4, num5) {
+//    return num1 + num2 + num3 + num4 + num5;
+//}
+
+function sum(...numbers) {
+    return numbers.reduce((a,b) => a + b);
+}
+
+
+console.log(sum(1, 2, 3, 4, 5));
