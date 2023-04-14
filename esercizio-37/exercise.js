692e3d020732695133fa67c56373a19b08247eee
Maurilio Farina <farinamaurilio93@gmail.com>
2023-04-14 20:27:21 +0200

    es 37:complete

---
+++ esercizio-37/exercise.js
@@ -0,0 +1,14 @@
+const number = 15;
+
+const myPromise = new Promise((resolve,reject) => {
+    if (number > 10) {
+        resolve("resolved");
+    } else {
+        reject("rejected");
+    }
+});
+
+myPromise
+  .then((resolve) => console.log(resolve))
+  .catch((error) => console.log(error))
+
